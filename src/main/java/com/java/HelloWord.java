cc514085d02451ed0316cfb3753394ebcc9c5aaf
admin <365996178@qq.com>
2021-03-28 00:03:18 +0800

    首次提交

---
+++ src/main/java/com/java/HelloWord.java
@@ -0,0 +1,12 @@
+package com.java;
+
+/*
+ * 开发者：jgp
+ * 时间：2021/3/27
+ * 描述：测试git
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("欢迎来到gittest");
+    }
+}
